2d1a45912c941add1e84077ef43cb618ded76b7f
SwiftSwaff <jeff.geyer.29@gmail.com>
2022-01-10 22:52:48 -0800

    Adjustments to attempt to allow for public files to be served as https

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Web Crawler</title>
-        <link href="{{asset("css/app.css")}}" rel="stylesheet">
-        <script src="{{asset("js/app.js")}}"></script>
+        <link href="{{ asset("css/app.css", \App::environment() == 'production') }}" rel="stylesheet">
+        <script src="{{ asset("js/app.js", \App::environment() == 'production') }}"></script>
     </head>
     <body>
         <header></header>
